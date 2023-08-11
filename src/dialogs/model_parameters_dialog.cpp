6d0636a89a1fd1d985b7c9ae931c00dc9a357661
Ian O'Rourke <cessna.ian@gmail.com>
2023-08-10 21:37:02 -0500

    Updates to parameter dialog

--- src/dialogs/model_parameters_dialog.cpp
+++ src/dialogs/model_parameters_dialog.cpp
@@ -16,7 +16,7 @@ ModelParametersDialog::ModelParametersDialog(std::shared_ptr<tmdl::Model> model,
 {
     ui->setupUi(this);
 
-    ui->nameLineEdit->setText(QString(model->get_name().c_str()));
+    ui->nameLabel->setText(QString(model->get_name().c_str()));
     ui->descriptionTextEdit->document()->setPlainText(QString(model->get_description().c_str()));
     ui->dtLineEdit->setText(QString("%1").arg(model->get_preferred_dt()));
 
@@ -27,14 +27,6 @@ void ModelParametersDialog::accept()
 {
     const auto& model_lib = tmdl::LibraryManager::get_instance().default_model_library();
 
-    std::string new_name = ui->nameLineEdit->text().toStdString();
-
-    if (model_lib->has_block(new_name) && new_name != model->get_name())
-    {
-        QMessageBox::warning(this, "error", QString("Name %1 already exists in library").arg(new_name.c_str()));
-        return;
-    }
-
     bool success = false;
     const double dtVal = ui->dtLineEdit->text().toDouble(&success);
 
@@ -44,7 +36,6 @@ void ModelParametersDialog::accept()
         return;
     }
 
-    const std::string orig_name = model->get_name();
     const std::string orig_desc = model->get_description();
     const double orig_dt = model->get_preferred_dt();
 

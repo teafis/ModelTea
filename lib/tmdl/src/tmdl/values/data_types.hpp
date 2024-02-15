f1ed574e31d118ab63368cd33dd2d80dff61ccbe
Ian O'Rourke <cessna.ian@gmail.com>
2024-02-14 21:34:23 -0600

    Working on consolidating stdlib models

--- lib/tmdl/src/tmdl/values/data_types.hpp
+++ lib/tmdl/src/tmdl/values/data_types.hpp
@@ -12,76 +12,11 @@
 
 namespace tmdl {
 
-enum class DataType : uint32_t {
-    UNKNOWN = 0,
-    SINGLE,
-    DOUBLE,
-    BOOLEAN,
-    INT32,
-    UINT32,
-    DATA_TYPE,
-    IDENTIFIER,
-};
+using DataType = mt::stdlib::DataType;
+template <DataType DT>
+using data_type_t = mt::stdlib::type_info<DT>;
 
-template <DataType> struct data_type_t {
-    static const bool has_value = false;
-    static const bool is_numeric = false;
-    static const bool is_modelable = false;
-};
-
-template <> struct data_type_t<DataType::SINGLE> {
-    static const bool has_value = true;
-    static const bool is_numeric = true;
-    static const bool is_modelable = true;
-    static const mt::stdlib::DataType mt_data_type = mt::stdlib::DataType::F32;
-    using type = mt::stdlib::type_info<mt_data_type>::type_t;
-};
-
-template <> struct data_type_t<DataType::DOUBLE> {
-    static const bool has_value = true;
-    static const bool is_numeric = true;
-    static const bool is_modelable = true;
-    static const mt::stdlib::DataType mt_data_type = mt::stdlib::DataType::F64;
-    using type = mt::stdlib::type_info<mt_data_type>::type_t;
-};
-
-template <> struct data_type_t<DataType::INT32> {
-    static const bool has_value = true;
-    static const bool is_numeric = true;
-    static const bool is_modelable = true;
-    static const mt::stdlib::DataType mt_data_type = mt::stdlib::DataType::I32;
-    using type = mt::stdlib::type_info<mt_data_type>::type_t;
-};
-
-template <> struct data_type_t<DataType::UINT32> {
-    static const bool has_value = true;
-    static const bool is_numeric = true;
-    static const bool is_modelable = true;
-    static const mt::stdlib::DataType mt_data_type = mt::stdlib::DataType::U32;
-    using type = mt::stdlib::type_info<mt_data_type>::type_t;
-};
-
-template <> struct data_type_t<DataType::BOOLEAN> {
-    static const bool has_value = true;
-    static const bool is_numeric = false;
-    static const bool is_modelable = true;
-    static const mt::stdlib::DataType mt_data_type = mt::stdlib::DataType::BOOL;
-    using type = mt::stdlib::type_info<mt_data_type>::type_t;
-};
-
-template <> struct data_type_t<DataType::DATA_TYPE> {
-    using type = DataType;
-    static const bool has_value = true;
-    static const bool is_numeric = false;
-    static const bool is_modelable = false;
-};
-
-template <> struct data_type_t<DataType::IDENTIFIER> {
-    using type = tmdl::Identifier;
-    static const bool has_value = true;
-    static const bool is_numeric = false;
-    static const bool modelable = false;
-};
+// TODO - Class-based data type to provide primitives + identifiers + etc?
 
 std::string data_type_to_string(const DataType dtype);
 
